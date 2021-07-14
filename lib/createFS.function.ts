9179c27674d076609e9b112b8b38310c33835eff
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-07-14 20:09:47 +0200

    fix: full names

--- lib/createFS.function.ts
+++ lib/createFS.function.ts
@@ -28,23 +28,23 @@ export function createFS({
       rootDirectoryName,
     });
 
-    const trans = db.transaction(storeName, type);
-    return trans.objectStore(storeName);
+    const transaction = db.transaction(storeName, type);
+    return transaction.objectStore(storeName);
   };
 
   async function writeFile<TData = any>(fileName: string, data: TData): Promise<void> {
     const objectStore = await initializeObjectStore('readwrite');
 
     return new Promise((resolve, reject) => {
-      const req = objectStore.put({
+      const request = objectStore.put({
         data,
         type: 'file',
         path: fileName,
         dir: path.dirname(fileName),
       });
 
-      req.onerror = reject;
-      req.onsuccess = () => resolve();
+      request.onerror = reject;
+      request.onsuccess = () => resolve();
     });
   }
 
