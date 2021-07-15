11f378b40ac3f6051ede010c6e30e756c3331b0d
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2021-07-15 19:06:28 +0200

    ✨ feat: create and read functions for directory management (#8)

--- lib/utils/initialize-database.function.ts
+++ lib/utils/initialize-database.function.ts
@@ -2,33 +2,51 @@ import { openIndexedDBConnection } from '.';
 
 import { CreateFSProps } from '../createFS.types';
 
+import { OBJECT_STORE_INDEX_NAME, OBJECT_STORE_KEY_PATH } from '../constants';
+
+function getDatabaseObjectFromTarget(target: EventTarget | null): IDBDatabase | null {
+  if (!target) {
+    return null;
+  }
+
+  const targetWithType = target as IDBOpenDBRequest;
+
+  return targetWithType.result;
+}
+
+function throwDatabaseOpenError(reject: (reason: any) => void, database: IDBDatabase | null) {
+  if (!database) {
+    reject(new Error('Something went wrong and the database transaction was not opened.'));
+  }
+}
+
 export const initializeDatabase = ({
-  storeName,
   databaseName,
-  indexedDBVersion,
-  rootDirectoryName,
-}: CreateFSProps): Promise<IDBDatabase> =>
+  databaseVersion,
+  objectStoreName,
+}: Required<CreateFSProps>): Promise<IDBDatabase> =>
   new Promise((resolve, reject) => {
-    const req = openIndexedDBConnection(databaseName, indexedDBVersion);
+    const req = openIndexedDBConnection(databaseName, databaseVersion);
 
     req.onupgradeneeded = ({ target }: IDBVersionChangeEvent) => {
-      const targetWithType = target as IDBOpenDBRequest;
-      const db = targetWithType.result;
+      const database = getDatabaseObjectFromTarget(target) as any;
 
-      if (!db || !storeName || !rootDirectoryName) {
-        reject(new Error('Something went wrong and the database transaction was not opened.'));
-      }
+      throwDatabaseOpenError(reject, database);
 
-      const stringStoreName = storeName || '';
-      const stringRootDirectoryName = rootDirectoryName || '';
+      const objectStore = database.createObjectStore(objectStoreName, {
+        keyPath: OBJECT_STORE_KEY_PATH,
+      });
 
-      const objectStore = db.createObjectStore(stringStoreName, { keyPath: 'path' });
-      objectStore.createIndex(stringRootDirectoryName, 'dir', { unique: false });
+      objectStore.createIndex(OBJECT_STORE_INDEX_NAME, OBJECT_STORE_INDEX_NAME, { unique: false });
     };
 
     req.onsuccess = ({ target }: Event) => {
-      const targetWithType = target as IDBOpenDBRequest;
+      const database = getDatabaseObjectFromTarget(target);
 
-      resolve(targetWithType.result);
+      throwDatabaseOpenError(reject, database);
+
+      resolve(getDatabaseObjectFromTarget(target) as IDBDatabase);
     };
+
+    req.onerror = reject;
   });
