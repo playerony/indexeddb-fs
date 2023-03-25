2b4aeb25d4d5b6a6c8e3bb0b017be278ddf91f0c
playerony <pawel.wojtasinski.1995@gmail.com>
2023-03-25 19:21:57 +0100

    refactor: rename all decorators to instances

--- lib/framework/parts/directory-details-instance/directory-details-instance.types.ts
+++ lib/framework/parts/directory-details-instance/directory-details-instance.types.ts
@@ -1,6 +1,5 @@
 export interface IDirectoryDetailsInstanceProps {
   getRecord: <TValue>(query: IDBValidKey | IDBKeyRange, onResolve: (target: IDBRequest) => TValue) => Promise<TValue>;
-
   isDirectory: (fullPath: string) => Promise<boolean>;
   rootDirectoryName: string;
 }
